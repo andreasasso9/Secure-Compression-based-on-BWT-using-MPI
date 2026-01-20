e2f005e640efc0f403de294bf36659958d1e1060
andreasasso9 <andry-sasso@outlook.it>
2026-01-20 20:06:16 +0100

    parallelizzata i-rle in decompression_mpi.py

--- src/decompression.py
+++ src/decompression.py
@@ -9,120 +9,122 @@ import math
 
 """""
 def block_bwt(input, key, index, return_dict):
-    outputBWT = sbwt.ibwt_from_suffix(input, key)
-    return_dict[index] = outputBWT
+	outputBWT = sbwt.ibwt_from_suffix(input, key)
+	return_dict[index] = outputBWT
 """""
 
 def block_bwt(task):
-    input_block, key, index = task
-    return index, sbwt.ibwt_from_suffix(input_block, key)
+	input_block, key, index = task
+	return index, sbwt.ibwt_from_suffix(input_block, key)
 
 def decompressione(secret_key: str, mode: int):
-    start = time.time()
-    # leggo il dizionario salvato dalla bwt in fase di compressione
-    dictionaryFile = open("TestFiles/Output/outputDictBWT.txt", "rb")
-    dictionaryLines = dictionaryFile.read()
-    dictionaryStr = dictionaryLines.decode()
-    dictionaryFile.close()  
-    # IPC
-    pcStartTime = time.time()
+	start = time.time()
+	# leggo il dizionario salvato dalla bwt in fase di compressione
+	dictionaryFile = open("TestFiles/Output/outputDictBWT.txt", "rb")
+	dictionaryLines = dictionaryFile.read()
+	dictionaryStr = dictionaryLines.decode()
+	dictionaryFile.close()  
+	# IPC
+	pcStartTime = time.time()
 
-    encodedFile = open("TestFiles/Output/outputPC.txt", "rb")
-    if mode == 1:
-        encoded = encodedFile.read()
-    else:
-        encoded = pickle.load(encodedFile)
-    encodedFile.close() 
-    outputPC = pc.decompress(encoded, mode)
+	encodedFile = open("TestFiles/Output/outputPC.txt", "rb")
+	if mode == 1:
+		encoded = encodedFile.read()
+	else:
+		encoded = pickle.load(encodedFile)
+	encodedFile.close() 
+	outputPC = pc.decompress(encoded, mode)
 
-    pcElapsedTime = time.time() - pcStartTime
-    print(str(pcElapsedTime) + "  -> elapsed time of I-PC")
-    #print("OUTPUT", outputPC[:500])
+	pcElapsedTime = time.time() - pcStartTime
+	print(str(pcElapsedTime) + "  -> elapsed time of I-PC")
+	#print("OUTPUT", outputPC[:500])
 
-    # IRLE
-    '''rleFile = open("TestFiles/Output/outputRLE.txt", "r")
-    rleLines = rleFile.readlines()
-    rleString = ""
-    for val in rleLines:
-        rleString += val'''
-    rleStartTime = time.time()
-    
-    rleModule = rle.Rle()
-    rleDecodedString = rle.Rle.rle_decode(rleModule, data=outputPC)
-    #print(rleDecodedString)
+	# IRLE
+	'''rleFile = open("TestFiles/Output/outputRLE.txt", "r")
+	rleLines = rleFile.readlines()
+	rleString = ""
+	for val in rleLines:
+		rleString += val'''
+	rleStartTime = time.time()
+	
+	rleModule = rle.Rle()
+	rleDecodedString = rle.Rle.rle_decode(rleModule, data=outputPC)
+	with open("TestFiles/Output/rleDecodedString.txt", "w") as tempRLEfile:
+			tempRLEfile.write(rleDecodedString)
+	#print(rleDecodedString)
 
-    rleElapsedTime = time.time() - rleStartTime
-    print(str(rleElapsedTime) + "  -> elapsed time of I-RLE")
+	rleElapsedTime = time.time() - rleStartTime
+	print(str(rleElapsedTime) + "  -> elapsed time of I-RLE")
 
-    # IMTF
-    
-    mtfStartTime = time.time()
+	# IMTF
+	
+	mtfStartTime = time.time()
 
-    block_size = 1024 #1/2((math.log2(len(stringInput))/math.log2(len(dictionary)))) The real formula is this one
+	block_size = 1024 #1/2((math.log2(len(stringInput))/math.log2(len(dictionary)))) The real formula is this one
 
-    mtfList = rleDecodedString.split(",")
-    res = []
-    for i in mtfList:
-        res.append(int(i))
-    #mtfDecodedString = mtf.decode(res, dictionary=sorted(dictionaryStr))
-    mtfDecodedString = bmtf.secure_decode(res, sorted(dictionaryStr), secret_key, block_size)
-    #print("-----MTF: " + mtfDecodedString)
+	mtfList = rleDecodedString.split(",")
+	res = []
+	for i in mtfList:
+		res.append(int(i))
+	#mtfDecodedString = mtf.decode(res, dictionary=sorted(dictionaryStr))
+	mtfDecodedString = bmtf.secure_decode(res, sorted(dictionaryStr), secret_key, block_size)
+	#print("-----MTF: " + mtfDecodedString)
 
-    mtfElapsedTime = time.time() - mtfStartTime
-    print(str(mtfElapsedTime) + "  -> elapsed time of I-BMTF")
+	mtfElapsedTime = time.time() - mtfStartTime
+	print(str(mtfElapsedTime) + "  -> elapsed time of I-BMTF")
 
-    # IBWT
+	# IBWT
 
-    bwtStartTime = time.time()
+	bwtStartTime = time.time()
 
-    # Dividi in blocchi mtfDecodedString
-    #block_lenght = 1024*30 +1 # Deve essere la stessa usata in compressione +1 per l'EOF
+	# Dividi in blocchi mtfDecodedString
+	#block_lenght = 1024*30 +1 # Deve essere la stessa usata in compressione +1 per l'EOF
 
-    bFile = open("TestFiles/Output/bfile.txt", "r")
-    block_lenght = int(bFile.readline()) + 1 #add EOF
-    bFile.close()   
-    using_blocks = True
-    bwtDecodedString = []   
-    rFile = open("TestFiles/Output/rfile.txt", "r")
-    r = rFile.readline()
-    rFile.close()
-    
-    nproc = multiprocessing.cpu_count()
-    if using_blocks and len(mtfDecodedString) > nproc * 10:
-        print("block mode")
-        
-        tasks = []
-        num_tasks = len(mtfDecodedString) // block_lenght
-        chunksize = max(1, num_tasks // (nproc * 2)) #Definisco la dimensione dei chunk per ogni processo 
-        j = 0
-        for i in range(0, len(mtfDecodedString), block_lenght):
-            input_block = mtfDecodedString[i:i+block_lenght] 
-            tasks.append((input_block, r + secret_key, j)) #ogni task è un blocco da processare e il suo indice j
-            j += 1
+	bFile = open("TestFiles/Output/bfile.txt", "r")
+	block_lenght = int(bFile.readline()) + 1 #add EOF
+	bFile.close()   
+	using_blocks = True
+	bwtDecodedString = []   
+	rFile = open("TestFiles/Output/rfile.txt", "r")
+	r = rFile.readline()
+	rFile.close()
+	
+	nproc = multiprocessing.cpu_count()
+	if using_blocks and len(mtfDecodedString) > nproc * 10:
+		print("block mode")
+		
+		tasks = []
+		num_tasks = len(mtfDecodedString) // block_lenght
+		chunksize = max(1, num_tasks // (nproc * 2)) #Definisco la dimensione dei chunk per ogni processo 
+		j = 0
+		for i in range(0, len(mtfDecodedString), block_lenght):
+			input_block = mtfDecodedString[i:i+block_lenght] 
+			tasks.append((input_block, r + secret_key, j)) #ogni task è un blocco da processare e il suo indice j
+			j += 1
 
 
-        with  multiprocessing.Pool(nproc) as pool:
-            results = pool.imap_unordered(block_bwt, tasks, chunksize) # processa i task in parallelo, se finisce un task prende il successivo
+		with  multiprocessing.Pool(nproc) as pool:
+			results = pool.imap_unordered(block_bwt, tasks, chunksize) # processa i task in parallelo, se finisce un task prende il successivo
 
-            # ricostruzione ordinata
-            output = [None] * j
-            for index, res in results:
-                output[index] = res
+			# ricostruzione ordinata
+			output = [None] * j
+			for index, res in results:
+				output[index] = res
 
-            bwtDecodedString.extend("".join(output[i]) for i in range(0,j))
-    else:
-        print("full file mode")
-        bwtDecodedString = sbwt.ibwt_from_suffix(mtfDecodedString, secret_key)
+			bwtDecodedString.extend("".join(output[i]) for i in range(0,j))
+	else:
+		print("full file mode")
+		bwtDecodedString = sbwt.ibwt_from_suffix(mtfDecodedString, secret_key)
 
-    #print(bwtDecodedString)
-    outputBWTFile = open("TestFiles/Output/decompressed.txt", "wb")
-    outputBWTString = ""
-    for i in range(0, len(bwtDecodedString)):
-        outputBWTString += bwtDecodedString[i]
-    #outputBWTFile.write(str(outputBWTString))
-    outputBWTFile.write(outputBWTString.encode())
-    outputBWTFile.close()   
-    bwtElapsedTime = time.time() - bwtStartTime
-    print(str(bwtElapsedTime) + "  -> elapsed time of I-BWT")
+	#print(bwtDecodedString)
+	outputBWTFile = open("TestFiles/Output/decompressed.txt", "wb")
+	outputBWTString = ""
+	for i in range(0, len(bwtDecodedString)):
+		outputBWTString += bwtDecodedString[i]
+	#outputBWTFile.write(str(outputBWTString))
+	outputBWTFile.write(outputBWTString.encode())
+	outputBWTFile.close()   
+	bwtElapsedTime = time.time() - bwtStartTime
+	print(str(bwtElapsedTime) + "  -> elapsed time of I-BWT")
 
-    print(str(time.time() - start) + " -> elapsed time of decompression")
+	print(str(time.time() - start) + " -> elapsed time of decompression")
--- src/decompression_mpi.py
+++ src/decompression_mpi.py
@@ -10,105 +10,154 @@ import numpy as np
 from mpi4py import MPI
 
 def block_bwt(input, key, index, return_dict):
-    output = sbwt.ibwt_from_suffix(input, key)
-    return_dict[index] = output
+	output = sbwt.ibwt_from_suffix(input, key)
+	return_dict[index] = output
 
 
 def decompressione(secret_key: str, mode: int):
-    comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
-    rFile = open("TestFiles/Output/rfile.txt", "r")
-    r = rFile.readline()
-    if rank == 0:
-        start = time.time()
-        # leggo il dizionario salvato dalla bwt in fase di compressione
-        dictionaryFile = open("TestFiles/Output/outputDictBWT.txt", "rb")
-        dictionaryLines = dictionaryFile.read()
-        dictionaryStr = dictionaryLines.decode()
+	comm = MPI.COMM_WORLD
+	size = comm.Get_size()
+	rank = comm.Get_rank()
+	rFile = open("TestFiles/Output/rfile.txt", "r")
+	r = rFile.readline()
 
-        # IPC
-        pcStartTime = time.time()
+	displ = []
+	counts = []
+	sendbuf = []
+	if rank == 0:
+		start = time.time()
+		# leggo il dizionario salvato dalla bwt in fase di compressione
+		dictionaryFile = open("TestFiles/Output/outputDictBWT.txt", "rb")
+		dictionaryLines = dictionaryFile.read()
+		dictionaryStr = dictionaryLines.decode()
 
-        encodedFile = open("TestFiles/Output/outputPC.txt", "rb")
-        if mode == 1:
-            encoded = encodedFile.read()
-        else:
-            encoded = pickle.load(encodedFile)
+		# IPC
+		pcStartTime = time.time()
 
-        outputPC = pc.decompress(encoded, mode)
+		encodedFile = open("TestFiles/Output/outputPC.txt", "rb")
+		if mode == 1:
+			encoded = encodedFile.read()
+		else:
+			encoded = pickle.load(encodedFile)
 
-        pcElapsedTime = time.time() - pcStartTime
-        print(str(pcElapsedTime) + "  -> elapsed time of I-PC")
-        #print("OUTPUT", outputPC[:500])
+		outputPC = pc.decompress(encoded, mode)
 
-        # IRLE
-        '''rleFile = open("TestFiles/Output/outputRLE.txt", "r")
-        rleLines = rleFile.readlines()
-        rleString = ""
-        for val in rleLines:
-            rleString += val'''
-        rleStartTime = time.time()
-        
-        rleModule = rle.Rle()
-        rleDecodedString = rle.Rle.rle_decode(rleModule, data=outputPC)
-        #print(rleDecodedString)
+		pcElapsedTime = time.time() - pcStartTime
+		print(str(pcElapsedTime) + "  -> elapsed time of I-PC")
+		#print("OUTPUT", outputPC[:500])
 
-        rleElapsedTime = time.time() - rleStartTime
-        print(str(rleElapsedTime) + "  -> elapsed time of I-RLE")
+		# IRLE
+		'''rleFile = open("TestFiles/Output/outputRLE.txt", "r")
+		rleLines = rleFile.readlines()
+		rleString = ""
+		for val in rleLines:
+			rleString += val'''
+		rleStartTime = time.time()
 
-        # IMTF
-        
-        mtfStartTime = time.time()
 
-        block_size = 1024 #1/2((math.log2(len(stringInput))/math.log2(len(dictionary)))) The real formula is this one
+		# Accede a una block_length approssimata per non scorrrere l'intero outputPC
+		
+		block_length = math.floor(len(outputPC) / size)
+		print("Block length RLE: ", block_length)
+		displ.append(0)
+		seek = block_length - 1
+		for i in range(size):
+			if i < size - 1:
+				# Cerca l'ultima virgola nel blocco per non spezzare i numeri
+				while True:
+					if outputPC[seek] == ',':
+						counts.append(seek + 1 - displ[i]) 
+						displ.append(displ[i] + counts[i])
+						seek += block_length
+						break
+					else:
+						seek -= 1
+			else:
+				# Ultimo processo prende il resto
+				remaining = len(outputPC) - sum(counts)
+				counts.append(remaining)
+	
+		sendbuf = np.frombuffer(outputPC.encode('utf-8'), dtype='b')
+	# Scatters the blocks to all processes
 
-        mtfList = rleDecodedString.split(",")
-        res = []
-        for i in mtfList:
-            res.append(int(i))
-        #mtfDecodedString = mtf.decode(res, dictionary=sorted(dictionaryStr))
-        mtfDecodedString = bmtf.secure_decode(res, sorted(dictionaryStr), secret_key, block_size)
-        #print("-----MTF: " + mtfDecodedString)
+	block_length = comm.scatter(counts, root=0)
+	recvbuf = np.empty(block_length, dtype='b')
+	comm.Scatterv((sendbuf, counts, displ, MPI.BYTE), recvbuf, root=0)
+	recvbuf = recvbuf.tobytes().decode("utf-8")
+	recvbuf = recvbuf[:-1] if recvbuf.endswith(",") else recvbuf  # rimuove la virgola rimasta dalla divisione dei blocchi
 
-        mtfElapsedTime = time.time() - mtfStartTime
-        print(str(mtfElapsedTime) + "  -> elapsed time of I-BMTF")
+	rleModule = rle.Rle()
+	rleDecodedStringBlock = rle.Rle.parallel_rle_decode(rleModule, data=recvbuf)
+		#print(rleDecodedString)
 
-        # IBWT
-        bwtStartTime = time.time()
+	rleDecodedString = comm.gather(rleDecodedStringBlock, root=0)
 
-    # Distribuisco i blocchi della BWT
-    block_length = 0
-    displ = []
-    counts = []
-    sendbuf = []
-    if rank == 0:
-        sendbuf = np.frombuffer(mtfDecodedString.encode("utf-8"), dtype='b')
-        block_length = math.floor(len(sendbuf) / size)
-        displ.append(0)
-        # Calcolo counts e displ, counts contiene la lunghezza di ogni blocco, displ contiene gli spostamenti
-        for i in range(size):
-            if i < size - 1:
-                counts.append(block_length)
-                displ.append(displ[i] + counts[i])
-            else:
-                counts.append(len(sendbuf) - block_length * (size - 1))
-    block_length = comm.scatter(counts, root=0)
-    recvbuf = np.empty(block_length, dtype='b')
-    comm.Scatterv((sendbuf, counts, displ, MPI.BYTE), recvbuf, root=0)
-    recvbuf = recvbuf.tobytes().decode("utf-8")
-    outputBWT_block = sbwt.ibwt_from_suffix(recvbuf, r + secret_key)
-    outputBWT_block = ''.join(outputBWT_block)
-    outputBWT_block_list = comm.gather(outputBWT_block, root=0)
+	if not rank:
+		rleDecodedString = ''.join(rleDecodedString)
+		rleDecodedString = rleDecodedString[:-1]  # rimuove l'ultima virgola aggiunta in parallel rle decode
 
-    if rank == 0:
-        outputBWTFile = open("TestFiles/Output/decompressed.txt", "wb")
-        outputBWTString = ""
-        for element in outputBWT_block_list:
-            if element is not None:
-                outputBWTString += element
-        outputBWTFile.write(outputBWTString.encode())
-        bwtElapsedTime = time.time() - bwtStartTime
-        print(str(bwtElapsedTime) + "  -> elapsed time of I-BWT")
+		with open("TestFiles/Output/rleDecodedString_mpi.txt", "w") as tempRLEfile:
+			tempRLEfile.write(rleDecodedString)
+		
+		""" rleDecodedStringTest = rle.Rle.rle_decode(rleModule, data=outputPC)
+		with open("TestFiles/Output/rleDecodedString.txt", "w") as tempRLEfile:
+			tempRLEfile.write(rleDecodedStringTest) """
 
-        print(str(time.time() - start) + " -> elapsed time of decompression")
+		rleElapsedTime = time.time() - rleStartTime
+		print(str(rleElapsedTime) + "  -> elapsed time of I-RLE")
+
+		# IMTF
+		
+		mtfStartTime = time.time()
+
+		block_size = 1024 #1/2((math.log2(len(stringInput))/math.log2(len(dictionary)))) The real formula is this one
+
+		mtfList = rleDecodedString.split(",")
+		res = []
+		for i in mtfList:
+			res.append(int(i))
+		#mtfDecodedString = mtf.decode(res, dictionary=sorted(dictionaryStr))
+		mtfDecodedString = bmtf.secure_decode(res, sorted(dictionaryStr), secret_key, block_size)
+		#print("-----MTF: " + mtfDecodedString)
+
+		mtfElapsedTime = time.time() - mtfStartTime
+		print(str(mtfElapsedTime) + "  -> elapsed time of I-BMTF")
+
+		# IBWT
+		bwtStartTime = time.time()
+
+	# Distribuisco i blocchi della BWT
+	block_length = 0
+	displ = []
+	counts = []
+	sendbuf = []
+	if rank == 0:
+		sendbuf = np.frombuffer(mtfDecodedString.encode("utf-8"), dtype='b')
+		block_length = math.floor(len(sendbuf) / size)
+		displ.append(0)
+		# Calcolo counts e displ, counts contiene la lunghezza di ogni blocco, displ contiene gli spostamenti
+		for i in range(size):
+			if i < size - 1:
+				counts.append(block_length)
+				displ.append(displ[i] + counts[i])
+			else:
+				counts.append(len(sendbuf) - block_length * (size - 1))
+	block_length = comm.scatter(counts, root=0)
+	recvbuf = np.empty(block_length, dtype='b')
+	comm.Scatterv((sendbuf, counts, displ, MPI.BYTE), recvbuf, root=0)
+	recvbuf = recvbuf.tobytes().decode("utf-8")
+	outputBWT_block = sbwt.ibwt_from_suffix(recvbuf, r + secret_key)
+	outputBWT_block = ''.join(outputBWT_block)
+	outputBWT_block_list = comm.gather(outputBWT_block, root=0)
+
+	if rank == 0:
+		outputBWTFile = open("TestFiles/Output/decompressed.txt", "wb")
+		outputBWTString = ""
+		for element in outputBWT_block_list:
+			if element is not None:
+				outputBWTString += element
+		outputBWTFile.write(outputBWTString.encode())
+		bwtElapsedTime = time.time() - bwtStartTime
+		print(str(bwtElapsedTime) + "  -> elapsed time of I-BWT")
+
+		print(str(time.time() - start) + " -> elapsed time of decompression")
