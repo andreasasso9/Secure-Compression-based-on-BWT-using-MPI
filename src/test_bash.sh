32e86cf878f68c08d8d72df84659e156fedc529d
Mattia Giuseppe Giella <mattiagiuseppegiella@gmail.com>
2026-01-22 13:03:23 +0100

    Refactor test_bash.sh to iterate over multiple compression modes and improve output logging

--- src/test_bash.sh
+++ src/test_bash.sh
@@ -5,54 +5,62 @@ OUTFILE="output_test.txt"
 
 DIR="./TestFiles/Input"     # cartella con i file
 key="chiave"
-mode="0"
+modes=(0 1 2)  # Modalità di compressione
+# 0 = Huffman\n
+# 1 = Arithmetic Coding \n
+# 2 = LZW\n
+    
 
 # Svuota il file di output
 > "$OUTFILE"
 
-# Itera su tutti i file della directory
-for file in "$DIR"/*; do
-   
-    word=$(basename "$file")
+for mode in "${modes[@]}"; do
+    {
+        echo "=============================="
+        echo " Modalità di compressione: $mode"
+        echo "=============================="
+        } | tee -a "$OUTFILE"
+    # Itera su tutti i file della directory
+    for file in "$DIR"/*; do
+        word=$(basename "$file")
+        for i in {1..3}; do
+            {
+                echo "=============================="
+                echo " ITERAZIONE $i - tester_MPI - file: $word"
+                echo "=============================="
+            } | tee -a "$OUTFILE"
 
-    for i in {1..3}; do
+            mpirun --oversubscribe -np  "$NUM_CPU" python3 ./tester_mpi.py "$word" "$key" "$mode" 2>&1 | tee -a "$OUTFILE"
+
+            echo "" | tee -a "$OUTFILE"
+        done
+        echo "" | tee -a "$OUTFILE"
+        for i in {1..3}; do
+            {
+                echo "=============================="
+                echo " ITERAZIONE $i - tester - file: $word"
+                echo "=============================="
+            } | tee -a "$OUTFILE"
+
+            python3 ./tester.py "$word" "$key" "$mode" 2>&1 | tee -a "$OUTFILE"
+
+            echo "" | tee -a "$OUTFILE"
+            sleep 1
+        done
+        echo "" | tee -a "$OUTFILE"  
+
+        for i in {1..3}; do
         {
-            echo "=============================="
-            echo " ITERAZIONE $i - tester_MPI - file: $word"
-            echo "=============================="
-        } | tee -a "$OUTFILE"
+                echo "=============================="
+                echo " ITERAZIONE $i - tester_bzip - file: $word"
+                echo "=============================="
+            } | tee -a "$OUTFILE"
 
-        mpirun --oversubscribe -np  "$NUM_CPU" python3 ./tester_mpi.py "$word" "$key" "$mode" 2>&1 | tee -a "$OUTFILE"
-
-        echo "" | tee -a "$OUTFILE"
+            python3 ./tester_bzip.py "$word" 2>&1 | tee -a "$OUTFILE"
+            echo "" | tee -a "$OUTFILE"
+            sleep 1
+        done
+        echo "" | tee -a "$OUTFILE"   
     done
-    echo "" | tee -a "$OUTFILE"
-    for i in {1..3}; do
-        {
-            echo "=============================="
-            echo " ITERAZIONE $i - tester - file: $word"
-            echo "=============================="
-        } | tee -a "$OUTFILE"
-
-        python3 ./tester.py "$word" "$key" "$mode" 2>&1 | tee -a "$OUTFILE"
-
-        echo "" | tee -a "$OUTFILE"
-        sleep 1
-    done
-    echo "" | tee -a "$OUTFILE"  
-    
-    for i in {1..3}; do
-       {
-            echo "=============================="
-            echo " ITERAZIONE $i - tester_bzip - file: $word"
-            echo "=============================="
-        } | tee -a "$OUTFILE"
-
-        python3 ./tester_bzip.py "$word" 2>&1 | tee -a "$OUTFILE"
-        echo "" | tee -a "$OUTFILE"
-        sleep 1
-    done
-    echo "" | tee -a "$OUTFILE"   
-
 done
 echo "Test completati. Risultati salvati in $OUTFILE" | tee -a "$OUTFILE"
